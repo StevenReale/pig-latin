c38353f6de157e81bbeff276933ca31d4ef74662
StevenReale <112089327+StevenReale@users.noreply.github.com>
2022-10-09 14:10:32 -0400

    Add files via upload

---
+++ pig-latin.js
@@ -0,0 +1,24 @@
+function findFirstVowel (str) {
+  let index = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (/[aeiou]/.test(str[i])) {
+      break;
+    }
+    index++;
+  }
+  return index;
+}
+
+function translatePigLatin(str) {
+  const firstVowel = findFirstVowel(str);
+  console.log(firstVowel);
+  if (firstVowel === 0) {
+    str += "way";
+  } else {
+    return str.slice(firstVowel) + str.slice(0, firstVowel) + "ay";
+  }
+
+  return str;
+}
+
+console.log(translatePigLatin("rhythm"));
